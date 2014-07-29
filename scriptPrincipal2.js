d55effc43a2fcbb44a98c53b03cfaa152418354d
psanchez123 <paul.sanchez@utec.edu.pe>
2014-07-29 11:23:11 -0500

    Intentando colocar cuadro

--- scriptPrincipal2.js
+++ scriptPrincipal2.js
@@ -7,6 +7,7 @@ function configurarApp()
 	canvas.width=screen.availWidth;
 	dibujaFooter(canvas,ctx)
 
+
 }
 function dibujaFooter(canvas,contexto)
 {
@@ -14,6 +15,7 @@ function dibujaFooter(canvas,contexto)
 	contexto.moveTo(0,0);
 	contexto.quadraticCurveTo(0,-50,canvas.width-100,canvas.height-50)
 	contexto.fill();
+	border: solid;
 
 
 }
